8ee05a6774d2f8596a53fa82062e45d21e249707
oono0908 <damian_wkids@yahoo.co.jp>
2020-02-11 19:48:16 +0900

    Update video_uploader.rb

--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -5,8 +5,8 @@ class VideoUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
-  # storage :fog
-  # process resize_to_fit: [800, 800]
+  storage :fog
+  process resize_to_fit: [800, 800]
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
